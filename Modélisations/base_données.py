a03c4912dbaa966fc804ab75fc93f435ef259610
SARRAU-Arthur <sarrau.arthur@yahoo.com>
2024-05-26 23:35:22 +0200

    Quelques modifs rapport partie n(z) et calculs pression = f(altittude) + Base données chargement

--- Modélisations/base_données.py
+++ Modélisations/base_données.py
@@ -54,7 +54,7 @@ def fonction_mathématiques_coefficients():
                     a[3]])
     
     def coefficient_b(z):
-        b = [288.15, 216.5, 196.5, 138.9]
+        b = [288, 219, 196.5, 138.5]
         return np.piecewise(z,
                 [z < z_trop,
                     (z_trop <= z) & (z < z_strat1),
@@ -74,19 +74,22 @@ def fonction_mathématique_température_altitude():
         return a(z) * z + b(z)
     
     return température_altitude
-    
-def fonction_mathématique_quantité_matière_altitude():
-    """ Renvoie la fonction de luminance d'un corps noir de température T 
-        et en fonction de la longueur d'onde lambda_ """
-    
-    def quantité_matière_altitude(z):
-        """ Renvoie l'image d'une valeur lamba_ donnée à travers la fonction de luminance 
-        d'un corps noir de température T """
-        T = fonction_mathématique_température_altitude()
+ 
+def fonction_mathématique_pression_altitude():
+  
+    def pression_altitude(z):
         a, b = fonction_mathématiques_coefficients()
-        term_1 = lambda z: np.where((z_trop <= z) & (z < z_strat1),
-                            P_0 * np.exp((-g * M * z) / (R * T(z))),
-                            P_0 * (1 - a(z) * z / b(z)) ** (M * g / (R * a(z))))
+        return np.where((z_trop <= z) & (z < z_strat1),
+                        P_0 * np.exp((-M * g * z) / (R * T_T)),
+                        P_0 * (1 + a(z) * z / b(z)) ** (-M * g / (R * a(z))))
+            
+    return pression_altitude
+
+def fonction_mathématique_quantité_matière_altitude():
+
+    def quantité_matière_altitude(z):
+        T = fonction_mathématique_température_altitude()  
+        term_1 = fonction_mathématique_pression_altitude()
         term_2 = k_B * T(z)
         return term_1(z) / term_2
     
@@ -101,27 +104,39 @@ def chargement_données_HITRAN_complet_fonction_z():
     nombre_onde, k_abs = absorptionCoefficient_Lorentz(SourceTables = 'CO2', 
                                          Diluent = {'air': 1.0},
                                          HITRAN_units = False)
-    transmittance = k_abs * quad(fonction_mathématique_quantité_matière_altitude(), 0, h_max, \
-                                 limit = 10 ** 7, full_output = 1)[0]
+    intégrale_densité_moléculaire = quad(fonction_mathématique_quantité_matière_altitude(), 0, h_max, \
+                                    limit = 10 ** 7, full_output = 1)[0]
+    transmittance = (k_abs * 10E-4) * intégrale_densité_moléculaire
     nom_fichier = chemin_acces('Bases de données', 'CO2 Absorption fonction z HITRAN', 'csv')
     with open(nom_fichier, mode = 'w', newline = '') as fichier_csv:
-        for lignes in range(0,len(nombre_onde)):
+        for lignes in range(0, len(nombre_onde)):
             fichier_csv.write(str(nombre_onde[lignes]) + ' ; ' + str(transmittance[lignes]) + '\n')
     return None
 
 def chargement_données_HITRAN(nom_fichier):
-    """ Chargement données de la base donnée HITRAN en un tableau à 2 colonnes: 
+    """ Chargement données de la base donnée HITRAN en un tableau à 2 colonnes:
     taux transmission CO2 (pas en %) et longueur d'onde (en m). Préciser dans <nom_fichier>
     si l'on souhaite utiliser les données avec z constant ou en fonction de z """
     data = np.loadtxt(chemin_acces('Bases de données', nom_fichier, 'csv'), delimiter = ';')
     taux_CO2 = []
     nombre_onde = 1E-2 * data[:,0]
     for i in range(0,np.size(data[:,0])):
-        taux_CO2.append(data[i,1]) 
+        taux_CO2.append(data[i,1])
     return 1 / nombre_onde, taux_CO2
 
 def chargement_données():
     return chargement_données_HITRAN('CO2 Absorption fonction z HITRAN')
 
-# print(quad(fonction_mathématique_quantité_matière_altitude(), 0, z_trop, limit = 10 ** 7, full_output = 1)[0])
+def profile_quantité_matière_CO2_altitude():
+    z = np.linspace(0, h_max - 1)
+    n = fonction_mathématique_quantité_matière_altitude()
+    plt.xlabel("Altitude atmosphérique (en m)")
+    plt.ylabel("Quantité de matière (en mol)")
+    plt.scatter(z, n(z), marker = '+')
+    plt.grid(True)
+    plt.show()
+    return None
+
+profile_quantité_matière_CO2_altitude()
+# print(quad(fonction_mathématique_quantité_matière_altitude(), 0, h_max, limit = 10 ** 7, full_output = 1)[0]) 
 chargement_données_HITRAN_complet_fonction_z()
