ac898acea64230c992e26e4739c5ae3df678a2ce
SARRAU-Arthur <sarrau.arthur@yahoo.com>
2024-06-10 23:32:42 +0200

    Modifications du 10/06/2024: Tout fonctionne plus de bug informatique, par contre 2 pb de résultats = Intégrales de 0 à inf toujours nulles quand on quitte le domaine de la base de donnée + Données de taux_CO2 aberrantes pour modèle ISA

--- Modélisations/base_données.py
+++ Modélisations/base_données.py
@@ -3,6 +3,7 @@ from constantes import *
 from scipy.integrate import quad
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def chemin_acces(langue, lettre, extension):
     """ Raccourci pour aller chercher le fichier au format choisi 
@@ -12,7 +13,7 @@ def chemin_acces(langue, lettre, extension):
 def chargement_données_NIST():
     """ Chargement données de la base donnée NIST en deux tableaux: 
     taux transmission CO2 (pas en %) et longueur d'onde (en m) """
-    data = np.loadtxt(chemin_acces('Bases de données/NIST','CO2 Absorption NIST','csv'), delimiter = ';')
+    data = np.loadtxt(chemin_acces('Bases de données/NIST','CO2 Absorption','csv'), delimiter = ';')
     taux_CO2 = []
     longueur_onde = 1E-2 * 1 / data[:-1,0] # NaN dernière ligne, on exclu pour éviter erreurs à l'exécution
     for i in range(0,np.size(data[:,0]) - 1):
@@ -26,10 +27,12 @@ def chargement_données_HITRAN_complet_z_constant():
     db_begin('../Bases de données/HITRAN z constant') # Chargement données depuis site
     # fetch('CO2', 2, 1, 0.757, 19908.186) # Accès aux données molécule CO2 entre 5.0231e-7 m et 1.321e-2 m
     fetch('CO2', 2, 1, 500, 2500)
-    nombre_onde, coef = absorptionCoefficient_Lorentz(SourceTables = 'CO2', Diluent = {'air': 1.0}) 
-    nombre_onde, transmittance = transmittanceSpectrum(nombre_onde, coef)
+    nombre_onde, k_abs = absorptionCoefficient_Lorentz(SourceTables = 'CO2', 
+                                                      Diluent = {'air': 1.0}, 
+                                                      HITRAN_units = False) 
+    nombre_onde, transmittance = transmittanceSpectrum(nombre_onde, k_abs)
     longueur_onde = 1 / nombre_onde * 1E-2
-    nom_fichier = chemin_acces('Bases de données/HITRAN z constant', 'CO2 Absorption z constant HITRAN', 'csv')
+    nom_fichier = chemin_acces('Bases de données/HITRAN z constant', 'CO2 Absorption', 'csv')
     with open(nom_fichier, mode = 'w', newline = '') as fichier_csv:
         for lignes in range(0, len(longueur_onde)):
             fichier_csv.write(str(longueur_onde[lignes]) + ' ; ' + str(transmittance[lignes]) + '\n')
@@ -119,14 +122,15 @@ def chargement_données_HITRAN_complet_fonction_z():
     """ Chargement données de la base donnée depuis le site HITRAN.org
     et redistribution dans un fichier csv en deux colonnes:
     taux transmission CO2 (pas en %) et nombre d'onde (en m) """
-    db_begin('../Bases de données/HITRAN fonction z') # Chargement données depuis site
+    db_begin('../Bases de données/HITRAN fonction z')
     fetch('CO2', 2, 1, 500, 2500)
-    nombre_onde, k_abs = absorptionCoefficient_Lorentz(SourceTables = 'CO2', Diluent = {'air': 1.0})
+    nombre_onde, k_abs = absorptionCoefficient_Lorentz(SourceTables = 'CO2', 
+                                                       Diluent = {'air': 1.0})
     intégrale_densité_moléculaire = quad(fonction_mathématique_quantité_matière_altitude(), 0, z_meso, \
                                     limit = 10 ** 7, full_output = 1)[0]
-    transmittance = 1 - (k_abs * 10E-4 * CO2_fraction) * intégrale_densité_moléculaire
+    transmittance = 1 - (k_abs * 1E-4 * CO2_fraction) * intégrale_densité_moléculaire
     longueur_onde = 1 / nombre_onde * 1E-2
-    nom_fichier = chemin_acces('Bases de données/HITRAN fonction z', 'CO2 Absorption fonction z HITRAN', 'csv')
+    nom_fichier = chemin_acces('Bases de données/HITRAN fonction z', 'CO2 Absorption', 'csv')
     with open(nom_fichier, mode = 'w', newline = '') as fichier_csv:
         for lignes in range(0, len(longueur_onde)):
             fichier_csv.write(str(longueur_onde[lignes]) + ' ; ' + str(transmittance[lignes]) + '\n')
@@ -141,7 +145,13 @@ def chargement_données_HITRAN(nom_fichier):
     return longueur_onde, taux_CO2
 
 def chargement_données():
-    return chargement_données_HITRAN('HITRAN fonction z/CO2 Absorption fonction z HITRAN')
+    # return chargement_données_HITRAN('HITRAN fonction z/CO2 Absorption')
+    return chargement_données_HITRAN('HITRAN z constant/CO2 Absorption')
 
 # chargement_données_HITRAN_complet_z_constant()
 # chargement_données_HITRAN_complet_fonction_z()
+
+# longueur_onde, taux_CO2 = chargement_données_HITRAN('HITRAN fonction z/CO2 Absorption')
+# print(min(longueur_onde), max(longueur_onde))
+# plt.plot(longueur_onde, taux_CO2, linewidth = 0.75, c = 'b')
+# plt.show()
--- Modélisations/modèle.py
+++ Modélisations/modèle.py
@@ -12,7 +12,8 @@ from os import system, name
 
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings 
+import warnings
+import time
 
 # Signatures et implémentation de fonctions
     
@@ -61,40 +62,69 @@ def fonction_transmittance_vers_absorbance(transmittance):
     return absorbance
 
 def intégrale(fonction, borne_inf, borne_sup): 
-    return quad(fonction, borne_inf, borne_sup, limit = 10 ** 7, full_output = 1)
+    return quad(fonction, borne_inf, borne_sup, full_output = 1)
 
 def spectre_transmission_CO2 (données_abscisses, données_ordonnées):
     """ Représentation graphique spectre transmission CO2 en fonction de la longueur d'onde """
     plt.plot(données_abscisses, données_ordonnées * 100, linewidth = 0.7)
-    plt.title("Taux de transmission du CO2 en fonction de la longueur d'onde")
     plt.xlabel("Longueur d'onde (en m)")
     plt.ylabel("Taux de transmission du CO2 (en %)")
     plt.grid(True)
     plt.show()
     return None
 
-def profile_fonction_altitude():
-    z = np.linspace(0, z_meso - 1)
-    fonction_P = fonction_mathématique_pression_altitude()
-    # fonction_T = fonction_mathématique_température_altitude()
-    # fonction_n = fonction_mathématique_quantité_matière_altitude()
-    P = np.array([fonction_P(z_i) for z_i in z])
-    # T = np.array([fonction_T(z_i) for z_i in z])
-    # n =  np.array([fonction_n(z_i) for z_i in z])
+def éléments_graphe(grandeur):
     plt.xlabel("Altitude atmosphérique (en m)")
-    plt.ylabel("Pression atmosphérique (en Pa)")
-    plt.plot(z, pressure(z), marker = '*', label = 'Atmosphère isoterme')
-    plt.plot(z, P, marker = '+', label = 'Atmosphère Standard 1976')
     plt.legend()
     plt.axvline(x = z_trop, color = 'grey', linestyle = '--', linewidth = 0.85, label = str(z_trop))
-    plt.text(z_trop + 100, np.mean(T), 'Troposphère', rotation = 'vertical')
+    plt.text(z_trop + 100, np.mean(grandeur), 'Troposphère', rotation = 'vertical')
     plt.axvline(x = z_strat1, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_strat1)
-    plt.text(z_strat1 + 100, np.mean(T), 'Stratosphère 1', rotation = 'vertical')
+    plt.text(z_strat1 + 100, np.mean(grandeur), 'Stratosphère 1', rotation = 'vertical')
     plt.axvline(x = z_strat2, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_strat2)
-    plt.text(z_strat2 + 100, np.mean(T), 'Stratosphère 2', rotation = 'vertical')
+    plt.text(z_strat2 + 100, np.mean(grandeur), 'Stratosphère 2', rotation = 'vertical')
     plt.axvline(x = z_meso, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_meso)
-    plt.text(z_meso + 100, np.mean(T), 'Mesosphère 1', rotation = 'vertical')
+    plt.text(z_meso + 100, np.mean(grandeur), 'Mesosphère 1', rotation = 'vertical')
     plt.grid(True)
+    return None
+
+def profile_P_fonction_altitude():
+    z = np.linspace(0, z_meso - 1)
+    fonction_P = fonction_mathématique_pression_altitude()
+    P =  np.array([fonction_P(z_i) for z_i in z])
+    plt.ylabel("Pression (en Pa)")
+    plt.plot(z, pressure(z), marker = '*', label = 'Atmosphère isoterme')
+    plt.plot(z, P, marker = '+', label = 'Atmosphère Standard 1976')
+    éléments_graphe(P)
+    plt.show()
+    return None
+
+def profile_T_fonction_altitude():
+    z = np.linspace(0, z_meso - 1)
+    fonction_T = fonction_mathématique_température_altitude()
+    T = np.array([fonction_T(z_i) for z_i in z])
+    plt.xlabel("Température (en K)")
+    plt.plot(T, z, marker = '+')
+    plt.ylabel("Altitude atmosphérique (en m)")
+    plt.axhline(y = z_trop, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_trop)
+    plt.text(np.mean(T), z_trop + 100, 'Troposphère')
+    plt.axhline(y = z_strat1, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_strat1)
+    plt.text(np.mean(T), z_strat1 + 100, 'Stratosphère 1')
+    plt.axhline(y = z_strat2, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_strat2)
+    plt.text(np.mean(T), z_strat2 + 100, 'Stratosphère 2')
+    plt.axhline(y = z_meso, color = 'grey', linestyle = '--', linewidth = 0.85, label = z_meso)
+    plt.text(np.mean(T), z_meso + 100, 'Mesosphère 1')
+    plt.grid(True)
+    plt.show()
+    return None
+
+def profile_n_fonction_altitude():
+    z = np.linspace(0, z_meso - 1)
+    fonction_n = fonction_mathématique_quantité_matière_altitude()
+    n =  np.array([fonction_n(z_i) for z_i in z])
+    plt.ylabel("Densité particulaire volumique (en molec.m^-3)")
+    plt.plot(z, air_number_density(z), marker = '*', label = 'Atmosphère isoterme')
+    plt.plot(z, n, marker = '+', label = 'Atmosphère Standard 1976')
+    éléments_graphe(n)
     plt.show()
     return None
         
@@ -135,24 +165,26 @@ def affichage_physique (paramètre, M_0):
         
 # Programme principal
 
-""" Affichage """
+""" Paramétrage de l'affichage """
 system('cls' if name == 'nt' else 'clear')
 warnings.filterwarnings('ignore')
 
 """ Chargement données selon le modèle choisi """
 longueur_onde, taux_CO2 = chargement_données()
-print(longueur_onde, taux_CO2)
-
-spectre_transmission_CO2(longueur_onde, taux_CO2)
+# intervalle = np.linspace(min(longueur_onde), max(longueur_onde))
+# intervalle = np.linspace(1E-6, 20E-6)
+intervalle = np.linspace(1E-10, 1E10)
 
 """ Transformation vers fonctions mathématiques continues """
-
 CO2_transmittance = fonction_mathématique_interpolation_transmittance(longueur_onde, taux_CO2)
 CO2_absorbance = fonction_mathématique_interpolation_absorbance(longueur_onde, taux_CO2)
 
-exit()
-
-# profile_fonction_altitude()
+""" Affichage fonctions absorbance et transmittance parfaitement en opposition de phases """
+# transmittance = [CO2_transmittance(lambda_i) for lambda_i in intervalle]
+# plt.plot(intervalle, transmittance)
+# absorbance = [CO2_absorbance(lambda_i) for lambda_i in intervalle]
+# plt.plot(intervalle, absorbance)
+# plt.show()
 
 """ Affichage spectre tranmission CO2 """
 # spectre_transmission_CO2(longueur_onde, taux_CO2)
@@ -163,8 +195,12 @@ exit()
 # intervalle = np.linspace(1E-6, 1000E-6, 10000)
 # spectre_luminance_corps_noir(intervalle, T_T, 'Terre')
 
+""" Profiles T, P, n en fonction de l'altitude """
+profile_T_fonction_altitude()
+# profile_P_fonction_altitude()
+# profile_n_fonction_altitude()
+
 """ Calculs différents flux par système sous hypothèse de corps noirs """
-# Système Soleil
 T = T_S
 print(f'> Système Soleil: T = {T} K \n')
 luminance_corps_noir_Soleil = fonction_mathématique_corps_noir(T)
@@ -172,15 +208,28 @@ luminance_corps_noir_Soleil = fonction_mathématique_corps_noir(T)
 flux_émis_corps_noir_Soleil = (C_S * T ** 4, 0.00)
 affichage_physique('théorique', flux_émis_corps_noir_Soleil)
 
+p = (R_T / (2 * d_TS)) ** 2
+flux_soleil_entrée_atmosphère = flux_émis_corps_noir_Soleil[0] * p * (1 - a_T) / (4 * np.pi * R_T ** 2)
+print(flux_soleil_entrée_atmosphère)
+
 intégrande_absorbance_Soleil = produit_de_fonctions(CO2_absorbance, luminance_corps_noir_Soleil)
-M_0_absorbance_Soleil = intégrale(intégrande_absorbance_Soleil, 0, np.inf)
-affichage_physique('absorbance', M_0_absorbance_Soleil) # Flèche 1
-
 intégrande_transmittance_Soleil = produit_de_fonctions(CO2_transmittance, luminance_corps_noir_Soleil)
-M_0_transmittance_Soleil = intégrale(intégrande_transmittance_Soleil, 0, np.inf) * (1 - a_T)
-affichage_physique('transmittance', M_0_transmittance_Soleil) # Flèche 2
 
-# Système Terre
+start_time = time.time()
+M_0_absorbance_Soleil = intégrale(intégrande_absorbance_Soleil, min(longueur_onde), max(longueur_onde))
+end_time = time.time()
+execution_time = end_time - start_time
+affichage_physique('absorbance', M_0_absorbance_Soleil) # Flèche 1
+print(f"Temps d'exécution: {execution_time} secondes")
+
+start_time = time.time()
+M_0_transmittance_Soleil = intégrale(intégrande_transmittance_Soleil, min(longueur_onde), max(longueur_onde))
+end_time = time.time()
+M_0_transmittance_Soleil = (M_0_transmittance_Soleil[0] * (1 - a_T), M_0_transmittance_Soleil[1]) 
+execution_time = end_time - start_time
+affichage_physique('transmittance', M_0_transmittance_Soleil) # Flèche 2
+print(f"Temps d'exécution: {execution_time} secondes")
+
 T = T_T
 print(f'> Système Terre: T = {T} K \n')
 luminance_corps_noir_Terre = fonction_mathématique_corps_noir(T)
@@ -188,30 +237,33 @@ luminance_corps_noir_Terre = fonction_mathématique_corps_noir(T)
 flux_émis_corps_noir_Terre = (C_S * T ** 4, 0.00)
 affichage_physique('théorique', flux_émis_corps_noir_Terre)
 
-plt.plot(longueur_onde, CO2_absorbance(longueur_onde))
-plt.plot(longueur_onde, CO2_transmittance(longueur_onde))
-plt.show()
 intégrande_absorbance_Terre = produit_de_fonctions(CO2_absorbance, luminance_corps_noir_Terre)
-M_0_absorbance_Terre = intégrale(intégrande_absorbance_Terre, 0, np.inf)
-affichage_physique('absorbance', M_0_absorbance_Terre) # Flèche 3
-
 intégrande_transmittance_Terre = produit_de_fonctions(CO2_transmittance, luminance_corps_noir_Terre)
-M_0_transmittance_Terre = intégrale(intégrande_transmittance_Terre, 0, np.inf)
-affichage_physique('transmittance', M_0_transmittance_Terre) # Flèche 4
 
-# Système Atmosphère
+start_time = time.time()
+M_0_absorbance_Terre = intégrale(intégrande_absorbance_Terre, min(longueur_onde), max(longueur_onde))
+end_time = time.time()
+execution_time = end_time - start_time
+affichage_physique('absorbance', M_0_absorbance_Terre) # Flèche 3
+print(f"Temps d'exécution: {execution_time} secondes")
+
+start_time = time.time()
+M_0_transmittance_Terre = intégrale(intégrande_transmittance_Terre, min(longueur_onde), max(longueur_onde))
+end_time = time.time()
+execution_time = end_time - start_time
+affichage_physique('transmittance', M_0_transmittance_Terre) # Flèche 4
+print(f"Temps d'exécution: {execution_time} secondes")
+
+exit()
+
 print(f'\n> Système Atmosphère: T = {T} K \n')
 
 M_0_atmosphère = M_0_absorbance_Terre[0] / 2
 affichage_physique('transmittance & absorbance', M_0_atmosphère) # Flèches 5, 6
 
 """ Bilan thermique """
-tolérance = 10
-
 émis = flux_émis_corps_noir_Soleil
 affichage_physique('Émis', émis)
 
 absorbé = M_0_transmittance_Terre[0] + M_0_transmittance_Soleil[0]
 affichage_physique('Absorbé', absorbé)
-
-print(f'Conclusion modèle: {np.abs(émis - absorbé) < tolérance}')
